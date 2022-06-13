2abdc8bff65566aead09fbad1857e8b564775419
coskunomer <coskunomer77@gmail.com>
2022-06-13 04:41:24 +0300

    3rd week hw solutions

---
+++ reconstruct/submission.py
@@ -0,0 +1,131 @@
+import shell
+import util
+import wordsegUtil
+
+############################################################
+# Problem 1b: Solve the segmentation problem under a unigram model
+
+class SegmentationProblem(util.SearchProblem):
+    def __init__(self, query, unigramCost):
+        self.query = query
+        self.unigramCost = unigramCost
+
+    def startState(self):
+        # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
+        return 0
+        # END_YOUR_CODE
+
+    def isEnd(self, state):
+        # BEGIN_YOUR_CODE (our solution is 2 lines of code, but don't worry if you deviate from this)
+        return state == len(self.query)
+        # END_YOUR_CODE
+
+    def succAndCost(self, state):
+        # BEGIN_YOUR_CODE (our solution is 7 lines of code, but don't worry if you deviate from this)
+        word = self.query[state:]
+        results = []
+        for s in range(len(word)):
+            newWord = word[:s+1]
+            results.append((newWord, state + s + 1, self.unigramCost(newWord)))
+        return results
+        # END_YOUR_CODE
+
+def segmentWords(query, unigramCost):
+    if len(query) == 0:
+        return ''
+
+    ucs = util.UniformCostSearch(verbose=0)
+    ucs.solve(SegmentationProblem(query, unigramCost))
+
+    # BEGIN_YOUR_CODE (our solution is 3 lines of code, but don't worry if you deviate from this)
+    return " ".join(ucs.actions)
+    # END_YOUR_CODE
+
+############################################################
+# Problem 2b: Solve the vowel insertion problem under a bigram cost
+
+class VowelInsertionProblem(util.SearchProblem):
+    def __init__(self, queryWords, bigramCost, possibleFills):
+        self.queryWords = queryWords
+        self.bigramCost = bigramCost
+        self.possibleFills = possibleFills
+
+    def startState(self):
+        # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
+        return self.queryWords[0], 1
+        # END_YOUR_CODE
+
+    def isEnd(self, state):
+        # BEGIN_YOUR_CODE (our solution is 2 lines of code, but don't worry if you deviate from this)
+        return state[1] == len(self.queryWords)
+        # END_YOUR_CODE
+
+    def succAndCost(self, state):
+        # BEGIN_YOUR_CODE (our solution is 8 lines of code, but don't worry if you deviate from this)
+        previousFill, state = state
+        word = self.queryWords[state]
+        possibleFills = self.possibleFills(word)
+        results = []
+        for fill in possibleFills:
+            results.append((fill, (fill, state+1), self.bigramCost(previousFill, fill)))
+        if len(results) == 0:
+            return [(word, (word, state+1), self.bigramCost(previousFill, word))]
+        return results
+        # END_YOUR_CODE
+
+def insertVowels(queryWords, bigramCost, possibleFills):
+    # BEGIN_YOUR_CODE (our solution is 3 lines of code, but don't worry if you deviate from this)
+    if len(queryWords) == 0: return ""
+    queryWords = [wordsegUtil.SENTENCE_BEGIN] + queryWords
+    ucs = util.UniformCostSearch(verbose=1)
+    ucs.solve(VowelInsertionProblem(queryWords, bigramCost, possibleFills))
+    return " ".join(ucs.actions)
+    # END_YOUR_CODE
+
+############################################################
+# Problem 3b: Solve the joint segmentation-and-insertion problem
+
+class JointSegmentationInsertionProblem(util.SearchProblem):
+    def __init__(self, query, bigramCost, possibleFills):
+        self.query = query
+        self.bigramCost = bigramCost
+        self.possibleFills = possibleFills
+
+    def startState(self):
+        # BEGIN_YOUR_CODE (our solution is 1 line of code, but don't worry if you deviate from this)
+        return (self.query[0], 1)
+        # END_YOUR_CODE
+
+    def isEnd(self, state):
+        # BEGIN_YOUR_CODE (our solution is 2 lines of code, but don't worry if you deviate from this)
+        return state[1] == len(self.query)
+        # END_YOUR_CODE
+
+    def succAndCost(self, state):
+        # BEGIN_YOUR_CODE (our solution is 14 lines of code, but don't worry if you deviate from this)
+        previousFill, state = state
+        word = "".join(self.query[state:])
+        results = []
+        for s in range(1, len(word)+1):
+            subWord = word[:s]
+            possibleFills = self.possibleFills(subWord)
+            for fill in possibleFills:
+                results.append((fill, (fill, state + s), self.bigramCost(previousFill, fill)))
+        return results
+        # END_YOUR_CODE
+
+def segmentAndInsert(query, bigramCost, possibleFills):
+    if len(query) == 0:
+        return ''
+
+    # BEGIN_YOUR_CODE (our solution is 4 lines of code, but don't worry if you deviate from this)
+    queryWords = [wordsegUtil.SENTENCE_BEGIN] + list(query)
+    ucs = util.UniformCostSearch(verbose=1)
+    ucs.solve(JointSegmentationInsertionProblem(queryWords, bigramCost, possibleFills))
+    return " ".join(ucs.actions)
+    # END_YOUR_CODE
+
+############################################################
+
+if __name__ == '__main__':
+    shell.main()
